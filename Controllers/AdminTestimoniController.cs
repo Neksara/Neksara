33730ebfc0bba55d630cde35785a4ed343e4c83a
fiorenzaa <fiorenza.laila24@gmail.com>
2026-01-07 20:26:23 +0700

    Menyelesaikan Admin Page (masih kurang filter, text rich document dan fungsionalitas pagination)

---
+++ Controllers/AdminTestimoniController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Neksara.Controllers
+{
+  public class AdminTestimoniController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
